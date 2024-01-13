5ac46f822a3b1aa687c33500d729cbe19774c21f
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-01-13 09:26:05 +0530

    added icons as components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import { Button } from 'react-bootstrap';
 import './App.css';
 
+import GithubIcon from './components/GitHubIcon';
+import GmailIcon from './components/GmailIcon';
+import LinkedinIcon from './components/LinkedinIcon';
+import InstagramIcon from './components/InstagramIcon';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         ISURU YAHAMPATH
 
+        <span className="Sub-header">
+          Ruru ルール
+
+          <Button className="Icon-button" name="gmail">
+              <GmailIcon />
+          </Button>
+
+          <Button className="Icon-button" name="github">
+
+          </Button>
+
+          <Button className="Icon-button" name="linkedin">
+
+          </Button>
+          
+          <Button className="Icon-button" name="instagram">
+          
+          </Button>
+        </span>
+
         <div className="Button-grid">
           <Button className="Grid-Button" name="about">
-              About me
+            About me
           </Button>
-          
+
           <Button className="Grid-Button" name="projects">
-              Projects
+            Projects
           </Button>
-          
+
           <Button className="Grid-Button" name="cv">
-              CV
+            CV
           </Button>
         </div>
       </header>
